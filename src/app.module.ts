f777e5b5e58d23cea8b76beb1f836e11ce27b947
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2022-11-30 22:59:11 -0300

    feat: Implemented CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BookModule } from './module/book/book.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule],
   controllers: [],
   providers: [],
 })
