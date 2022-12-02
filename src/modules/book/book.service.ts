3316dbe09aad9d3f29b0267422e1b49ec4382cf8
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2022-12-01 22:47:34 -0300

    feat: implementing Book Service

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,8 +1,25 @@
+import { PrismaService } from './../../database/PrismaService';
 import { BookDTO } from './book.dto';
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class BookService {
-  async create(data: BookDTO) {}
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: BookDTO) {
+    const bookExists = await this.prisma.book.findFirst({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
+
+    if (bookExists) {
+      throw new Error('Book already exists');
+    }
+
+    const book = await this.prisma.book.create.prototype({
+      data,
+    });
+    return book;
+  }
 }
