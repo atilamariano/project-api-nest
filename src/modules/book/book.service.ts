2e39cdbdd80f93f2064f7df33957b76dc44d442f
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2022-12-02 22:49:22 -0300

    feat: implementing CRUD finished

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,6 +1,6 @@
-import { PrismaService } from './../../database/PrismaService';
-import { BookDTO } from './book.dto';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/PrismaService';
+import { BookDTO } from './book.dto';
 
 @Injectable()
 export class BookService {
@@ -17,9 +17,51 @@ export class BookService {
       throw new Error('Book already exists');
     }
 
-    const book = await this.prisma.book.create.prototype({
+    const book = await this.prisma.book.create({
       data,
     });
+
     return book;
+  }
+
+  async findAll() {
+    return this.prisma.book.findMany();
+  }
+
+  async update(id: string, data: BookDTO) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!bookExists) {
+      throw new Error('Book does not exists!');
+    }
+
+    return await this.prisma.book.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async delete(id: string) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!bookExists) {
+      throw new Error('Book does not exists!');
+    }
+
+    return await this.prisma.book.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
